df1800ad19bf8beeb426cd3d52522dab2daee923
Daniel Xavier Araujo <danielxaraujo@gmail.com>
2018-04-27 23:49:33 -0300

    Duplicate Style

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import { ScrollView, Text, Button } from 'react-native';
 
 export default class DetailScreen extends Component {
+	static navigatorStyle = {
+		largeTitle: true,
+		topBarElevationShadowEnabled: true,
+		statusBarTextColorScheme: 'light',
+		navBarBackgroundColor: '#3498db',
+		navBarButtonColor: '#FFFFFF'
+	}
 	render() {
 		return (
 			<ScrollView contentContainerStyle={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
