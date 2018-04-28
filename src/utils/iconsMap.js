c8ce116db0ba22d2cdc7ad22d7d93c0049fa2b4a
Daniel Xavier Araujo <danielxaraujo@gmail.com>
2018-04-27 23:04:27 -0300

    Change to 4 tabs

--- src/utils/iconsMap.js
+++ src/utils/iconsMap.js
@@ -1,24 +1,24 @@
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const icons = {
-    'bars': { icon: 'bars', size: 24, color: '#FFFFFF', provider: FontAwesome }
+	'bars': { icon: 'bars', size: 24, color: '#FFFFFF', provider: FontAwesome }
 }
 
 export const iconsMap = {};
 export const iconsLoaded = new Promise((resolve, reject) => {
-    new Promise.all(
-        Object.keys(icons).map(iconName => {
-            const icon = icons[iconName];
-            return icon.provider.getImageSource(
-                icon.icon,
-                icon.size,
-                icon.color
-            )
-        })
-    ).then(sources => {
-        Object.keys(icons).forEach((iconName, idx) => iconsMap[iconName] = sources[idx]);
-        resolve(true);
-    })
+	new Promise.all(
+		Object.keys(icons).map(iconName => {
+			const icon = icons[iconName];
+			return icon.provider.getImageSource(
+				icon.icon,
+				icon.size,
+				icon.color
+			)
+		})
+	).then(sources => {
+		Object.keys(icons).forEach((iconName, idx) => iconsMap[iconName] = sources[idx]);
+		resolve(true);
+	})
 })
 
 export default iconsMap;
